cab4a10ad3bc3c83098a6260c91f86ef6fbd4708
Appin Design <appindes@gmail.com>
2018-09-02 12:57:44 +0100

    Initial commit

---
+++ dib/build/generated/source/buildConfig/test/debug/com/appindesign/dib/test/BuildConfig.java
@@ -0,0 +1,18 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.appindesign.dib.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.appindesign.dib.test";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 7;
+  public static final String VERSION_NAME = "1.7";
+  /**
+   * @deprecated Use {@link #APPLICATION_ID}
+   */
+  @Deprecated
+  public static final String PACKAGE_NAME = "com.appindesign.dib.test";
+}
